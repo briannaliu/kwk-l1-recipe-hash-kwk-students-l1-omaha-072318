06be743f7cc65f723df0c69503deffb3be2f193f
briannaliu <briannaliu3@gmail.com>
2018-07-26 20:31:30 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -17,12 +17,12 @@ def recipe_ingredients(perfect_10_ingredients)
 perfect_10_recipe = {}
 perfect_10_recipe = {
 :almond_flour => "2 cups", 
-:gluten_freewhole_oats => "1 cup", 
+:gluten_free_whole_oats => "1 cup", 
 :kosher_salt => "1/2 tea spoon", 
 :baking_powder => "1/2 tea spoon", 
 :baking_soda => "1/4 tea spoon", 
 :xanthan_Gum => "1/2 tea spoon", 
-:slivered_almonds_lightly_toasted => "1/4 cup", 
+:slivered_almonds => "1/4 cup", 
 :mini_dark_chocolate_chips => "3/4 cup", 
 :olive_oil => "1/2 cup", 
 :agave => "1/4 cup"
